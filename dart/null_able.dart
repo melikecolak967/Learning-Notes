8767bd00eddf3461d4905d02e8b6afde0c638427
Melike <165141178+melikecolak967@users.noreply.github.com>
2025-06-29 15:48:51 +0300

    Update null_able.dart

--- dart/null_able.dart
+++ dart/null_able.dart
@@ -1,13 +1,18 @@
 void main(){
-// The Dart compiler prohibits null dereference errors. In other languages, this could lead to a runtime error.
+  // The Dart compiler prohibits null dereference errors. In other languages, this could lead to a runtime error.
+  
+  // Dart catches such errors at compile-time, preventing runtime errors.
+  int? i;
+  print(i.abs()); // Error! i might be null, so abs() cannot be called.
+  
+  // To access properties or methods of a nullable variable, you must first check that it is not null.
+  String? text;
+  if (text != null) {
+    print(text.length); // Safe
+  }
 
-// Dart catches such errors at compile-time, preventing runtime errors.
-int? i;
-print(i.abs()); // Error! i might be null, so abs() cannot be called.
-
-// To access properties or methods of a nullable variable, you must first check that it is not null.
-String? text;
-if (text != null) {
-  print(text.length); // Safe
-}
+  // Types like int? and String? can be null. Their default value is null if no value is assigned.
+  int? lineCount;
+  print(lineCount); // null
+  
 }
